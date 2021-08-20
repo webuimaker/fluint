090d8592fafae2fd3869a3e8c73c3bfd1d9b437b
webuimaker <webuimaker@gmail.com>
2021-08-20 16:36:44 +0530

    firstst

---
+++ assets/js/script.js
@@ -0,0 +1,182 @@
+/* ========================================================================= */
+/*	Page Preloader
+/* ========================================================================= */
+
+$(window).on('load', function () {
+	$('.preloader').fadeOut(100);
+});
+
+jQuery(function ($) {
+	"use strict";
+
+	// ------------------------------------------------------------------------------ //
+	// Navbar Fixed
+	// ------------------------------------------------------------------------------ //
+	$(window).on("scroll", function(){
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop >200){
+			$("nav.attoums-nav").removeClass("no-background");
+		}else {
+			$("nav.attoums-nav").addClass("no-background");
+		}
+	});
+            
+
+
+	
+    /*--------------------------
+        STICKY MAINMENU
+    ---------------------------*/
+    $("#mainmenu-area").sticky({
+        topSpacing: 0
+    });
+
+    /*---------------------------
+        SMOOTH SCROLL
+    -----------------------------*/
+    $('ul#nav li a[href^="#"], a.navbar-brand, a.scrolltotop').on('click', function (event) {
+        var id = $(this).attr("href");
+        var offset = 60;
+        var target = $(id).offset().top - offset;
+        $('html, body').animate({
+            scrollTop: target
+        }, 1500, "easeInOutExpo");
+        event.preventDefault();
+    });
+
+
+	/* ========================================================================= */
+	/*	lazy load initialize
+	/* ========================================================================= */
+
+	const observer = lozad(); // lazy loads elements with default selector as ".lozad"
+	observer.observe();
+
+	/* ========================================================================= */
+	/*	Magnific popup
+	/* =========================================================================  */
+	$('.image-popup').magnificPopup({
+		type: 'image',
+		removalDelay: 160, //delay removal by X to allow out-animation
+		callbacks: {
+			beforeOpen: function () {
+				// just a hack that adds mfp-anim class to markup
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+				this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+		closeOnContentClick: true,
+		midClick: true,
+		fixedContentPos: false,
+		fixedBgPos: true
+	});
+
+	/* ========================================================================= */
+	/*	Portfolio Filtering Hook
+	/* =========================================================================  */
+
+	var containerEl = document.querySelector('.shuffle-wrapper');
+	if (containerEl) {
+		var Shuffle = window.Shuffle;
+		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+			itemSelector: '.shuffle-item',
+			buffer: 1
+		});
+
+		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+			var input = evt.currentTarget;
+			if (input.checked) {
+				myShuffle.filter(input.value);
+			}
+		});
+	}
+
+	/* ========================================================================= */
+	/*	Testimonial Carousel
+	/* =========================================================================  */
+
+	$("#testimonials").slick({
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 4000
+	});
+
+	/* ========================================================================= */
+	/*	animation scroll js
+	/* ========================================================================= */
+
+
+
+	function myFunction(x) {
+		if (x.matches) {
+			var topOf = 50
+		} else {
+			var topOf = 350
+		}
+	}
+
+	var html_body = $('html, body');
+	$('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				html_body.animate({
+					scrollTop: target.offset().top - 50
+				}, 1500, 'easeInOutExpo');
+				return false;
+			}
+		}
+	});
+
+	// easeInOutExpo Declaration
+	jQuery.extend(jQuery.easing, {
+		easeInOutExpo: function (x, t, b, c, d) {
+			if (t === 0) {
+				return b;
+			}
+			if (t === d) {
+				return b + c;
+			}
+			if ((t /= d / 2) < 1) {
+				return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+			}
+			return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+		}
+	});
+
+	/* ========================================================================= */
+	/*	counter up
+	/* ========================================================================= */
+	function counter() {
+		var oTop;
+		if ($('.count').length !== 0) {
+			oTop = $('.count').offset().top - window.innerHeight;
+		}
+		if ($(window).scrollTop() > oTop) {
+			$('.count').each(function () {
+				var $this = $(this),
+					countTo = $this.attr('data-count');
+				$({
+					countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				}, {
+					duration: 1000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.floor(this.countNum));
+					},
+					complete: function () {
+						$this.text(this.countNum);
+					}
+				});
+			});
+		}
+	}
+	$(window).on('scroll', function () {
+		counter();
+	});
+
+});
---
+++ content/_index.md
@@ -0,0 +1,83 @@
+---
+title: 'fluentbit'
+description: ''
+hero:  
+  enabled: true
+  heading: High Performance
+  tag: "The most downloaded log forwarder & stream processor <br> for Linux, BSD, and OSX."
+  subHeading: less overload, go deeper,
+  btnText: DOWNLOAD NOW
+  btnUrl: "#"
+  version: v1.8.2 released on Jul 20, 2021
+  noteText: Read the notes
+  noteUrl: #
+howitworks: 
+  enabled: true
+  image: /images/how.svg   
+  btnText: Read how it works
+  btnUrl: "#"
+  info: Collect any data from different sources, enrich them with filters and send them to multiple destinations. 
+  lists: 
+  - Different sources of information
+  - Different data formats
+  - Data Reliability
+  - Security
+  - Flexible Routing
+  - Multiple destinations 
+brand: 
+  enabled: true 
+  heading: Trusted and inspired by unique brands
+  subHeading: 
+performance: 
+  enabled: true
+  heading: Designed with performance in mind
+  subHeading:  High throughput with low CPU and Memory usage. It's written in C language and has a pluggable architecture supporting more than 70 extensions for inputs, filters and outputs.
+  lists:
+  - heading: Lightweight
+    icon: /images/lightweight.svg
+    description: It has been designed as a lightweight solution with high performance in mind. From a design perspective, it's fully asynchronous (event-driven) and take the most of the operating systems API for performance and reliability.
+  - heading: Extensible
+    icon: /images/extensible.svg
+    description: All inputs, filters and outputs features are implemented through the plugins interface. Extend the features with C, Lua (filters) or Golang (outputs).
+  - heading: Metrics
+    icon: /images/metrics.svg
+    description: Meassuring is important. Fluent Bit comes with native plugins to gather metrics from your CPU, Memory, Disk I/O and Network usage on Linux systems. In addition, it can receive metrics from external services like Statsd and Collectd.
+testimonials:
+  enabled: true
+  description: Fluent Bit is a CNCF (Cloud Native Computing Foundation) subproject under the umbrella of Fluentd.
+      We are part of a wide community, no vendor lock-in.
+features:
+  enabled: true
+  heading: Relevant & Dynamic
+  subHeading:  Our features offer a new ecosystem to your flow, with improvements of    <br> greater permanence in high performance.
+  lists:
+  - heading: Event Driven
+    link: "#"
+    linkText: "Explore"
+    description: Fluent Bit as a service is fully event-driven, it only use asynchronous operations to collect and deliver data
+  - heading: Flexible Routing
+    link: "#"
+    linkText: "Explore"
+    description: The data that comes in the pipeline, can be routed to multiple places using custom routing rules. Ship your data to multiple places with zero-copy strategy.
+  - heading: Configuration
+    link: "#"
+    linkText: "Explore"
+    description: It configuration is very simple and human-readable, it allow to specify how it will behave, which features to enable and how Routing is performed.
+  - heading: I/O Handler
+    link: "#"
+    linkText: "Explore"
+    description: The Input/Output layer provides an abstraction that allow to perform read/write operations in an asynchronous way.
+  - heading: Upstream Manager
+    link: "#"
+    linkText: "Explore"
+    description: Our Upstream manager simplify the connection process and take care of timeout/network exceptions and Keepalive states.
+  - heading: Security & TLS
+    link: "#"
+    linkText: "Explore"
+    description: When delivering data to destinations, output connectors inherit full TLS capabilities in an abstracted way. Add your certificates as required.
+cta:
+  enabled: true
+  image: "/images/cta.png"
+  heading: Stay Updated
+  subHeading:  Leave your email and get connected with our lastest news, relases and more.
+---
