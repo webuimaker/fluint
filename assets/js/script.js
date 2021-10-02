28d36b382bebcb52ed040201a5a5aa14b7673943
webuimaker <webuimaker@gmail.com>
2021-10-02 11:55:19 +0530

    blog list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,83 @@
+
+
+/* ========================================================================= */
+/*	Type code Animation
+/* ========================================================================= */
+    function setupTypewriter(t) {
+      var HTML = t.innerHTML;
+
+      t.innerHTML = "";
+
+      var cursorPosition = 0,
+          tag = "",
+          writingTag = false,
+          tagOpen = false,
+          typeSpeed = 100,
+        tempTypeSpeed = 0;
+
+      var type = function() {
+        
+          if (writingTag === true) {
+              tag += HTML[cursorPosition];
+          }
+
+          if (HTML[cursorPosition] === "<") {
+              tempTypeSpeed = 0;
+              if (tagOpen) {
+                  tagOpen = false;
+                  writingTag = true;
+              } else {
+                  tag = "";
+                  tagOpen = true;
+                  writingTag = true;
+                  tag += HTML[cursorPosition];
+              }
+          }
+          if (!writingTag && tagOpen) {
+              tag.innerHTML += HTML[cursorPosition];
+          }
+          if (!writingTag && !tagOpen) {
+              if (HTML[cursorPosition] === " ") {
+                  tempTypeSpeed = 0;
+              }
+              else {
+                  tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+              }
+              t.innerHTML += HTML[cursorPosition];
+          }
+          if (writingTag === true && HTML[cursorPosition] === ">") {
+              tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+              writingTag = false;
+              if (tagOpen) {
+                  var newSpan = document.createElement("span");
+                  t.appendChild(newSpan);
+                  newSpan.innerHTML = tag;
+                  tag = newSpan.firstChild;
+              }
+          }
+
+          cursorPosition += 1;
+          if (cursorPosition < HTML.length - 1) {
+              setTimeout(type, tempTypeSpeed);
+          }
+
+      };
+
+      return {
+          type: type
+      };
+  }
+
+  var typer = document.getElementById('typewriter');
+
+  typewriter = setupTypewriter(typewriter);
+
+  typewriter.type();
+
+
+
+
+
 /* ========================================================================= */
 /*	Page Preloader
 /* ========================================================================= */
@@ -15,9 +95,10 @@ jQuery(function ($) {
 	$(window).on("scroll", function(){
 		var scrollTop = $(window).scrollTop();
 		if(scrollTop >200){
-			$("nav.attoums-nav").removeClass("no-background");
+			
+			// $(".fixed-top.bg-white").addClass("header-scrolled");
 		}else {
-			$("nav.attoums-nav").addClass("no-background");
+			// $(".fixed-top.bg-white").removeClass("header-scrolled");
 		}
 	});
             
